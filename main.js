4173e820d1f04eb3f9fc3848bf9f0f6f141fc124
rehkasigh <91784285+rehkasigh@users.noreply.github.com>
2022-08-19 06:03:37 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -8,11 +8,23 @@ function setup(){
     video=createCapture(VIDEO);
     video.size(300,300);
     video.hide();
-  
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 function modelLoaded(){
     console.log('poseNet is initilized');
 }
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        nosex=results[0].pose.nose.x-15;
+        nosey=results[0].pose.nose.y-15;
+        console.log("nose x="+results[0].pose.nose.x); 
+        console.log("nose y="+results[0].pose.nose.y);
+    }
+}
+  
+
 
 function take_snapshot(){
     save('myfilterimage.png');
